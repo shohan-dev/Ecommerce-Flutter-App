d71ebdc5b8b65c9966d44972f1fe080f00f2a307
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2024-07-03 07:06:22 +0600

    PageView added

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:t_store/features/authentication/controllers/onboading/obboading_controller.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
 import 'package:t_store/utils/theme/theme.dart';
 
@@ -7,25 +9,19 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       // Changed Scaffold to MaterialApp for full app setup
       title: 'Your App Title', // Replace with your app's title
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme, // Replace with your light theme
       darkTheme: TAppTheme.darkTheme, // Replace with your dark theme
-      debugShowCheckedModeBanner: true, // Set to false for production
+      debugShowCheckedModeBanner: false, // Set to false for production
       // initialBinding: GeneralBindings(), // Uncomment if using GetX bindings
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(TTexts.appName), // Example app bar with title
-        ),
-        body: Center(
-          child: Text(
-            TTexts.onBoardingSubTitle1,
-            textAlign: TextAlign.center,
+          appBar: AppBar(
+            title: Text(TTexts.appName), // Example app bar with title
           ),
-        ),
-      ),
+          body: OnBoardingScreen()),
     );
   }
 }
