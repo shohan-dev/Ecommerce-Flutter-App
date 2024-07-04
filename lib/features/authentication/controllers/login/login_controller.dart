f35001333a828a523eba81eb98da2a1b30b9386c
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2024-07-04 22:22:20 +0600

    logion page login added

--- lib/features/authentication/controllers/login/login_controller.dart
+++ lib/features/authentication/controllers/login/login_controller.dart
@@ -1,1 +1,15 @@
+import 'package:get/get.dart';
 
+class LoginPageController extends GetxController {
+  static LoginPageController get instance => Get.find();
+
+  // Variables
+
+  void formpage() {}
+  void rememberme() {}
+  void forgotpassword() {}
+  void signinbutton() {}
+  void createbutton() {}
+  void signinwithgoogle() {}
+  void signinwithfacebook() {}
+}
